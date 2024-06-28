8f9df3366e9516451a019d2917489b79e5542cac
Victor Wangari <victorwangari65187@gmail.com>
2024-06-28 14:46:02 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -21,7 +21,29 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
 
+    else:
+        body = {"message" : f"Earthquake {id} not found."}
+        status = 404
+    response = make_response(body,status)
+    return response
+
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def get_by_minimum(magnitude):
+    earthquakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude>=magnitude).all():
+        earthquakes.append(earthquake.to_dict())
+
+    body = {"count" : len(earthquakes),
+                "quakes" : earthquakes
+                }
+    return make_response(body,200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
